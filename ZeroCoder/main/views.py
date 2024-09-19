bcf1e568f8c869f8ad0d8ab8e46c9d3056a169e2
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-19 18:14:33 +0500

    mod django project

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,11 +1,16 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>This is my first Django project!</h1>")
+    data = {
+        'caption':"CatDjango"
+    }
+    # return HttpResponse("<h1>This is my first Django project!</h1>")
+    return render(request, 'main/index.html', data)
 
 def new(request):
-    return HttpResponse("<h1>This is second page of my Django project!</h1>")
+    # return HttpResponse("<h1>This is second page of my Django project!</h1>")
+    return render(request, 'main/new.html')
