543fe378526df1bece04dce5768d801f9a316899
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-20 14:23:34 +0500

    added news module

--- ZeroCoder/ZeroCoder/settings.py
+++ ZeroCoder/ZeroCoder/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#    BASE_DIR / "static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -21,5 +21,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('news/', include('news.urls'))
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
